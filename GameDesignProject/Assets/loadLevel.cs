ce58190c523ab9fa480b7f53fc031163a5f3853b
VladCircu <circuvlad98@gmail.com>
2021-05-27 02:16:17 +0300

    final project

---
+++ GameDesignProject/Assets/loadLevel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour
+{
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+
+    public bool useIntegerToLoadLevel = false;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if (collisionGameObject.name == "player")
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if (useIntegerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+        else
+        {
+            SceneManager.LoadScene(sLevelToLoad);
+        }
+    }
+
+
+}
+
